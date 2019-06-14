6dee547827d6c441c110685fb413d4debacfd132
Jules Kuehn <jk@jules.lol>
2019-06-14 16:40:44 -0400

    Clause tree functional (but ugly)

--- controllers/generatorController.js
+++ controllers/generatorController.js
@@ -30,12 +30,12 @@ exports.all_infos = (req, res, next) => {
     });
 };
 
-// Display the content of all informative sections
+// Display the content of all clauses
 exports.all_clauses = (req, res, next) => {
   Clause.find()
     .exec((err, list_clauses) => {
       if (err) { return next(err); }
-      list_clauses = list_clauses.sort( (a, b) => a.number.localeCompare(b.number, undefined, { numeric:true }) );
+      list_clauses = list_clauses.sort((a, b) => a.number.localeCompare(b.number, undefined, { numeric: true }));
       res.render('all_clauses', { title: strings.allClausesTitle, item_list: list_clauses });
     });
 };
@@ -46,10 +46,10 @@ exports.create_get = (req, res, next) => {
     presets: (callback) => Preset.find().sort([['order', 'ascending']]).exec(callback)
   }, (err, results) => {
     if (err) { return next(err); }
-    results.clauses = results.clauses.sort( (a, b) => a.number.localeCompare(b.number, undefined, { numeric:true }) );
+    results.clauses = toClauseTree(results.clauses);
     res.render('select_fps', {
       title: strings.createTitle,
-      item_list: results.clauses,
+      item_tree: results.clauses,
       preset_list: results.presets
     });
   });
@@ -91,7 +91,7 @@ exports.create_post = (req, res, next) => {
     if (results.fps == null) { // No clauses selected
       res.redirect('/view/create');
     }
-    results.fps = results.fps.sort( (a, b) => a.number.localeCompare(b.number, undefined, { numeric:true }) );
+    results.fps = results.fps.sort((a, b) => a.number.localeCompare(b.number, undefined, { numeric: true }));
     res.render('all_requirements', {
       title: strings.generatedRequirementsTitle,
       item_list: results.fps,
@@ -99,4 +99,66 @@ exports.create_post = (req, res, next) => {
       annex: results.annex
     });
   });
+};
+
+// Reformat clause array into nested dict (nested clauses in parents)
+/*
+tree = {
+  '5': {
+    clause: { number: '5' },
+    'children': {
+      '5.1': {
+        clause: { number: '5.1' },
+        'children': {
+          '5.1.1': {
+            clause: { number: '5.1.1' },
+            children: {}
+          }
+        }
+      }
+    }
+  }
+}
+*/
+const toClauseTree = (clauses) => {
+
+  clauses = clauses.sort((a, b) => a.number.localeCompare(b.number, undefined, { numeric: true }));
+  let clauseTree = {};
+
+  for (clause of clauses) {
+    let ancestry = clause.number.split(".");
+    let ancestors = [];
+
+    while (ancestry.length > 0) {
+      ancestors.push(ancestry.join('.'));
+      ancestry.pop();
+    }
+
+    ancestors.reverse();
+    let treeIndex = '["' + ancestors.join('"]["children"]["') + '"]';
+    let eString = `clauseTree` + treeIndex + ` = {
+      'clause': clause,
+      'children': {}
+    }`;
+    eval(eString);
+  }
+
+  // Convert children objects to sorted arrays
+  // No need for keys as they are duplicated in clause.number
+  const sortChildren = (tree) => {
+    // Base case
+    if (Object.keys(tree).length === 0 && tree.constructor === Object) {
+      return [];
+    }
+    // Convert top level of tree to array and sort
+    tree = Object.values(tree);
+    tree.sort((a, b) => a.clause.number.localeCompare(b.clause.number, undefined, { numeric: true }));
+    // Recurse on children of top level nodes
+    for (node of tree) {
+      node.children = sortChildren(node.children);
+    }
+    return tree;
+  }
+
+  return sortChildren(clauseTree);
 };
