8380da89e4420ac53b89f46402daed3cf782bfa5
Jules Kuehn <jk@jules.lol>
2019-06-20 14:42:53 -0400

    Recursive tree selection javascript (fully working)

--- public/a11y-req.js
+++ public/a11y-req.js
@@ -55,38 +55,43 @@ const setupTreeHandler = () => {
   // 1. Explicit selection of a parent clause cascades down sub-clauses
   // 2. Any selection of a sub-clause forces selection of parent clause
   // 3. Informative clauses must be checked whenever parent is checked
-  $('.checkbox input:checkbox').change(function() {
+  $('.checkbox input:checkbox').change(function () {
     let $el = $(this);
-    console.log($el);
-    
-    
+
     // Handle case 1: Cascade selection change down sub-clauses
     if ($el.closest('summary').length > 0) {
       $el.closest('details')
-          .find('.checkbox input:checkbox')
-          .prop('checked', $el.prop('checked'));
+        .find('.checkbox input:checkbox')
+        .prop('checked', $el.is(':checked'));
     }
 
     // Handle cases 2 and 3
     bubbleUpClause($el);
   });
-  
+
   let bubbleUpClause = ($el) => {
 
+    // Get parent clause, if there is one
     let parent = $el.closest('details');
     // If this sub-clause has children, the target needs adjustment
-    if ($el.closest('div.leafNode').length === 0 ) {
+    if ($el.closest('div.leafNode').length === 0) {
       parent = parent.parent();
+      // Reached the top of the clause tree. Nothing to do.
+      if (!parent.is('details')) {
+        return;
+      }
     }
 
-    // Parents must be checked if any child is checked
-    let value = parent.find('input:checkbox:not(:first)').is(':checked');
+    // Parents must be checked if any child is checked, and unchecked otherwise
+    let value = parent.find('input:checkbox:not(:first):not(.informative)').is(':checked');
     parent.find('summary .checkbox input:checkbox')
-        .first().prop('checked', value);
+      .first().prop('checked', value);
 
     parent.children('div.leafNode').find('input:checkbox.informative').prop('checked', value);
 
-  }    
+    // Recurse up the tree
+    bubbleUpClause(parent);
+  }
 
 };
 
@@ -113,7 +118,7 @@ function MinHeightPlugin(editor) {
   this.editor = editor;
 };
 
-MinHeightPlugin.prototype.init = function() {
+MinHeightPlugin.prototype.init = function () {
   this.editor.ui.view.editable.extendTemplate({
     attributes: {
       style: {
